9d6d7af30192a2c08ac43f23fe0511c25a91c149
florian7380 <florian_dupper@gmx.de>
2024-11-08 13:22:20 +0100

    Update stonks.py

--- stonks.py
+++ stonks.py
@@ -1,7 +1,8 @@
 """
-Dises Programm wurde mit Hilfe von ChatGPT geschrieben.
-Es werden automatisch Kursdaten verschiedener Wertpapiere herunter geladen und in Datenbanken eingepfflegt, die dann ausgelesen, verglichen und dargestellt werden können.
-Benötigt wird ein ein API-Key von AlphaVantage, Polygon IO und StockData. Der frei zugängliche genügt.
+Dieses Programm wurde mit Hilfe von ChatGPT geschrieben.
+Es werden automatisch Kursdaten verschiedener Wertpapiere heruntergeladen und in Datenbanken eingepflegt, die dann ausgelesen, verglichen und dargestellt werden können.
+Benötigt wird ein API-Key von AlphaVantage, Polygon IO und StockData. Der frei zugängliche genügt. Die entsprechende API-Keys müssen in den entsprechenden Threads eingefügt werden.
+Die Liste der dargestellten Daten ist unter List.TickerList und stark gekürzt, da der Vergleich schnell viel Leistung fordert.
 """
 
 from PyQt5.QtWidgets import QApplication
